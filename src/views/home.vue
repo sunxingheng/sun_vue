5aaa207f0b77b87e0d5c6792b4f954931a505596
sunxingheng <361789874@qq.com>
2017-12-28 21:53:33 +0800

    新增px2rem,新增flexible

---
+++ src/views/home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="home">
+		<div class="home_list">
+			123213
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="less">
+	.home{
+		.home_list{
+			width: 100%;
+			height: 400px;
+			background-color: #f00;
+			color: firebrick;
+		}
+	}
+</style>
